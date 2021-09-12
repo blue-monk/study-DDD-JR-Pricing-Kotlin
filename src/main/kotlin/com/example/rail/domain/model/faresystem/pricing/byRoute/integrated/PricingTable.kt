bce4447a140861ea720ebb564041db64f957d4d9
blue-monk <blue.monk.487@gmail.com>
2021-09-12 19:26:31 +0900

    refactor: ルート別の価格設定を1つの価格設定テーブルで管理
    * 新規ルート登録時の登録漏れリスク減少を期待して
    * ルート別の料金一覧性も期待

---
+++ src/main/kotlin/com/example/rail/domain/model/faresystem/pricing/byRoute/integrated/PricingTable.kt
@@ -0,0 +1,68 @@
+package com.example.rail.domain.model.faresystem.pricing.byRoute.integrated
+
+import com.example.rail.domain.model.faresystem._foundation.monetary.amount.FareAmount
+import com.example.rail.domain.model.faresystem.factor.route.Route
+import com.example.rail.domain.model.faresystem.factor.route.Station
+import kotlinx.collections.immutable.toImmutableMap
+
+private typealias B = FaresForRoute.BasicFare
+private typealias L = FaresForRoute.LimitedExpressReservedSeatFare
+private typealias N = FaresForRoute.NozomiPremiumChargeFare
+
+/**
+ * プライシングテーブル
+ *
+ * ルート別の価格設定
+ *
+ * - 普通運賃
+ * - 特急指定席料金
+ * - のぞみプレミアムチャージ料金
+ */
+object PricingTable {
+
+    private val faresByRoute = mapOf(
+
+            Route(Station.Tokyo,    Station.SinOsaka) to FaresForRoute(B( 8_910), L(5_490), N(320)),
+            Route(Station.Tokyo,    Station.Himeji)   to FaresForRoute(B(10_010), L(5_920), N(530)),
+            Route(Station.SinOsaka, Station.Himeji)   to FaresForRoute(B( 1_520), L(2_290), N(210)),
+
+    ).toImmutableMap()
+
+
+    private fun faresFor(route: Route): FaresForRoute {
+        return faresByRoute[route] ?: faresByRoute[route.reversed()] ?: throw IllegalStateException("当該 route の定義がありません [route=$route]")
+    }
+
+
+    fun basicFareFor(route: Route): FareAmount {
+        return faresFor(route).basicFare.fareAmount
+    }
+
+    fun limitedExpressReservedSeatFareFor(route: Route): FareAmount {
+        return faresFor(route).limitedExpressReservedSeatFare.fareAmount
+    }
+
+    fun nozomiPremiumChargeFareFor(route: Route): FareAmount {
+        return faresFor(route).nozomiPremiumChargeFare.fareAmount
+    }
+}
+
+private data class FaresForRoute(
+
+        val basicFare: BasicFare,
+        val limitedExpressReservedSeatFare: LimitedExpressReservedSeatFare,
+        val nozomiPremiumChargeFare: NozomiPremiumChargeFare
+) {
+
+    data class BasicFare(val fareAmount: FareAmount) {
+        constructor(amount: Int) : this(FareAmount(amount))
+    }
+
+    data class LimitedExpressReservedSeatFare(val fareAmount: FareAmount) {
+        constructor(amount: Int) : this(FareAmount(amount))
+    }
+
+    data class NozomiPremiumChargeFare(val fareAmount: FareAmount) {
+        constructor(amount: Int) : this(FareAmount(amount))
+    }
+}
